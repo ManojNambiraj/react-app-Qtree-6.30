1d288e3cb23c278c5d94c0690981dc64a7397aaa
manojn1106 <manojdeveloper1997@gmail.com>
2024-08-13 20:32:26 +0530

    Shopping cart day2 using JSON Data

--- src/Components/ProductCard.js
+++ src/Components/ProductCard.js
@@ -1,19 +1,16 @@
 import React from "react";
 import img1 from "../assets/images/mobile1.avif";
 
-function ProductCard() {
+function ProductCard(props) {
   return (
     <div class="card col-3">
-      <img src={img1} class="card-img-top" alt="..." />
+      <img src={props.item.img} class="card-img-top" alt="..." />
       <div class="card-body">
-        <h5 class="card-title">Card title</h5>
-        <p class="card-text">
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </p>
-        <a href="#" class="btn btn-primary">
-          Go somewhere
-        </a>
+        <h5 class="card-title">{props.item.name}</h5>
+        <h6>{props.item.price}</h6>
+        <p class="card-text">{props.item.desc}</p>
+        <p>{(props.item.isStock === true) ? "- in Stock" : "- out of stock"}</p>
+        <button class="btn btn-primary" disabled={!props.item.isStock}>Add to cart</button>
       </div>
     </div>
   );
--- src/pages/ShoppingCart.js
+++ src/pages/ShoppingCart.js
@@ -1,19 +1,69 @@
 import React from "react";
 import ProductCard from "../Components/ProductCard";
+import img1 from "../assets/images/mobile1.avif";
 
 function ShoppingCart() {
+
+  const datas = [
+    {
+      id: 1,
+      name: "iPhone",
+      price: 50000,
+      img: img1,
+      desc: "card title and make up the bulk of the card's content.",
+      isStock: true
+    },
+    {
+      id: 2,
+      name: "Moto",
+      price: 15000,
+      img: img1,
+      desc: "card title and make up the bulk of the card's content.",
+      isStock: false
+    },
+    {
+      id: 3,
+      name: "Mi",
+      price: 20000,
+      img: img1,
+      desc: "card title and make up the bulk of the card's content.",
+      isStock: true
+    },
+    {
+      id: 4,
+      name: "Sony",
+      price: 45000,
+      img: img1,
+      desc: "card title and make up the bulk of the card's content.",
+      isStock: true
+    },
+    {
+      id: 5,
+      name: "Samsung",
+      price: 17000,
+      img: img1,
+      desc: "card title and make up the bulk of the card's content.",
+      isStock: false
+    },
+    {
+      id: 6,
+      name: "Vivo",
+      price: 24000,
+      img: img1,
+      desc: "card title and make up the bulk of the card's content.",
+      isStock: true
+    },
+  ];
+  
+
   return (
     <div className="container-fluid">
       <div className="row">
         <div className="col-10">
           <div className="row d-flex">
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            <ProductCard />
-            
+            {datas.map((item) => {
+              return <ProductCard item={item} />;
+            })}
           </div>
         </div>
         <div className="col-2">right</div>
